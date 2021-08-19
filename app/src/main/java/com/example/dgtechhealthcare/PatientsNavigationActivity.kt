9b1b57563e7be49c3aa3dad36f095e3ae9879436
jasmeet0624 <jasmeet241195@icloud.com>
2021-08-19 15:32:13 +0530

    Patients Commit

---
+++ app/src/main/java/com/example/dgtechhealthcare/PatientsNavigationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dgtechhealthcare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientsNavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patients_navigation)
+    }
+}
