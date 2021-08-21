5b2abc245c54c2777649526e3f02d73e43339438
RohitSuhag7 <rohitsuhag7>
2021-08-21 16:33:30 +0530

    Changes in Nurse Section

---
+++ app/src/main/java/com/example/dgtechhealthcare/nurse/model/ProfileData.kt
@@ -0,0 +1,6 @@
+package com.example.dgtechhealthcare.nurse.model
+
+data class ProfileData(val username: String ?= null,
+                       val contact: String ?= null,
+                       val hospital: String ?= null,
+                       val profileImage: String ?= null)
