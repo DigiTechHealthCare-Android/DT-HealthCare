f85310d147a404b9e3b17b6dc81a3e9f9e254668
Aryan Agarwal <hansy.agarwal@gmail.com>
2021-08-25 19:35:41 +0530

    basic content manager done. add edit delete articles

--- app/src/main/java/com/example/dgtechhealthcare/contentManager/ContentMangerShowContentFragment.kt
+++ app/src/main/java/com/example/dgtechhealthcare/contentManager/ContentMangerShowContentFragment.kt
@@ -1,26 +1,25 @@
 package com.example.dgtechhealthcare.contentManager
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
+import android.os.Bundle
+import android.view.*
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dgtechhealthcare.R
-import com.example.dgtechhealthcare.patientInfo.PatientInfoDataClass
 import com.example.dgtechhealthcare.utils.FirebasePresenter
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerSupportFragment
-import com.google.android.youtube.player.YouTubePlayerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -38,15 +37,30 @@ class ContentMangerShowContentFragment(val view: View) {
         val firebaseRecyclerAdapter : FirebaseRecyclerAdapter<ContentDataClass,ContentViewHolder> =
             object : FirebaseRecyclerAdapter<ContentDataClass,ContentViewHolder>(options){
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentViewHolder {
-
                     val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_content_manger_show_content,parent,false)
                     return ContentViewHolder(view)
                 }
-
                 override fun onBindViewHolder(holder: ContentViewHolder,
                     position: Int, model: ContentDataClass) {
 
                     val userID = getRef(position).key
+
+                    holder.edit.setOnClickListener {
+                        val options = arrayOf("Edit","Delete")
+                        val builder = AlertDialog.Builder(activity)
+                        builder.setTitle("Do you want to?")
+                        builder.setItems(options,DialogInterface.OnClickListener { dialog, which ->
+                            if(which == 0){
+                                editContent(userID)
+                            }
+                            if(which ==1){
+                                deleteContent(userID)
+                            }
+                        })
+                        builder.show()
+                    }
+
+
                     reference.articleReference.child(userID!!).addValueEventListener(object : ValueEventListener{
                         override fun onDataChange(snapshot: DataSnapshot) {
                             if(snapshot.hasChild("type")){
@@ -77,49 +91,6 @@ class ContentMangerShowContentFragment(val view: View) {
                                         ) {}
 
                                     })
-
-                                    /*var yplayer : YouTubePlayer? = null
-
-                                    val player = YouTubePlayerSupportFragment.newInstance()
-                                    val transaction : FragmentTransaction = activity.supportFragmentManager.beginTransaction()
-                                    transaction.add(R.id.youtube_fragment,player).commit()
-                                    player.initialize("AIzaSyB9O8Gvi7gpkfsI2gKIImhVDnZODsTYiK4",object : YouTubePlayer.OnInitializedListener{
-                                        override fun onInitializationSuccess(
-                                            p0: YouTubePlayer.Provider?,
-                                            p1: YouTubePlayer?,
-                                            p2: Boolean
-                                        ) {
-                                            if(!p2){
-                                                yplayer = p1
-                                                yplayer?.setFullscreen(false)
-                                                yplayer?.loadVideo("HyHNuVaZJ-k")
-                                                yplayer?.play()
-                                            }
-                                        }
-
-                                        override fun onInitializationFailure(
-                                            p0: YouTubePlayer.Provider?,
-                                            p1: YouTubeInitializationResult?
-                                        ) {}
-
-                                    })*/
-                                    //val frag : YouTubePlayerSupportFragment = activity.supportFragmentManager.findFragmentById(R.id.youtube_player_fragment)
-
-
-
-                                    /*holder.video.initialize("AIzaSyB9O8Gvi7gpkfsI2gKIImhVDnZODsTYiK4",object : YouTubePlayer.OnInitializedListener{
-
-                                        override fun onInitializationSuccess(p0: YouTubePlayer.Provider?,
-                                            p1: YouTubePlayer?, p2: Boolean) {
-                                            p1?.cueVideo("HyHNuVaZJ-k")
-                                        }
-
-                                        override fun onInitializationFailure(p0: YouTubePlayer.Provider?,
-                                            p1: YouTubeInitializationResult?) {
-
-                                        }
-
-                                    })*/
                                 } else if(snapshot.child("type").value.toString().compareTo("research")==0){
                                     val title = snapshot.child("title").value.toString()
                                     val imageRef = snapshot.child("imageRef").value.toString()
@@ -152,10 +123,27 @@ class ContentMangerShowContentFragment(val view: View) {
                                 }
                             }
                         }
-
                         override fun onCancelled(error: DatabaseError) {}
-
                     })
+                }
+
+                private fun deleteContent(userID: String?) {
+                    reference.articleReference.child(userID!!).removeValue().addOnCompleteListener {
+                        if(it.isSuccessful){
+                            reference.managerReference.child(reference.currentUserId!!).child("articles").child(userID!!).removeValue().addOnCompleteListener {
+                                if(it.isSuccessful) Toast.makeText(activity,"Article Deleted",Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+                }
+
+                private fun editContent(userID: String?) {
+                    val frag = EditArticlesFragment()
+                    val bundle = Bundle()
+                    bundle.putString("userID",userID)
+                    frag.arguments = bundle
+                    activity.supportFragmentManager.beginTransaction()
+                        .replace(R.id.contentFrame,frag).addToBackStack(null).commit()
                 }
 
             }
@@ -163,14 +151,15 @@ class ContentMangerShowContentFragment(val view: View) {
         activity.registerForContextMenu(recyclerView)
         recyclerView.adapter = firebaseRecyclerAdapter
         firebaseRecyclerAdapter.startListening()
-    }
 
+    }
 
     inner class ContentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val title = itemView.findViewById<TextView>(R.id.contentShowTitle)
         val desc = itemView.findViewById<TextView>(R.id.contentShowDesc)
         val image = itemView.findViewById<ImageView>(R.id.contentShowImg)
         val cardView = itemView.findViewById<CardView>(R.id.contentShowCV)
+        val edit = itemView.findViewById<ImageButton>(R.id.contentEdit)
         //val video = itemView.findViewById<FrameLayout>(R.id.youtube_fragment)
     }
 }
---
+++ app/src/main/java/com/example/dgtechhealthcare/contentManager/EditArticlesFragment.kt
@@ -0,0 +1,138 @@
+package com.example.dgtechhealthcare.contentManager
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.example.dgtechhealthcare.R
+import com.example.dgtechhealthcare.utils.FirebasePresenter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.HashMap
+
+class EditArticlesFragment : Fragment() {
+
+    lateinit var title : EditText
+    lateinit var contentImg : ImageView
+    lateinit var contentDesc : EditText
+    lateinit var contentUrl : EditText
+    lateinit var contentRG : RadioGroup
+    lateinit var publishB : Button
+
+    lateinit var reference : FirebasePresenter
+    var galleryPick : Int = 0
+    var imgUri : Uri = Uri.parse("")
+
+    var contentUid = ""
+    var type1 = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {}
+
+        contentUid = arguments?.getString("userID","")!!
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_articles, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeValues(view)
+
+        reference.articleReference.child(contentUid).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val type = snapshot.child("type").value.toString()
+                for(i in 0..contentRG.childCount-1){
+                    contentRG.getChildAt(i).isEnabled = false
+                }
+                if(type.compareTo("image")==0){
+                    contentRG.check(R.id.contentImageRE)
+                    title.setText(snapshot.child("title").value.toString())
+                    contentDesc.setText(snapshot.child("desc").value.toString())
+                    contentUrl.setText(snapshot.child("url").value.toString())
+                    contentImg.setImageURI(Uri.parse(snapshot.child("imageRef").value.toString()))
+                } else if(type.compareTo("video")==0){
+                    contentRG.check(R.id.contentVideoRE)
+                    title.setText(snapshot.child("title").value.toString())
+                    contentUrl.setText(snapshot.child("url").value.toString())
+                } else if(type.compareTo("research")==0){
+                    contentRG.check(R.id.contentResearchRE)
+                    title.setText(snapshot.child("title").value.toString())
+                    contentDesc.setText(snapshot.child("desc").value.toString())
+                    contentUrl.setText(snapshot.child("url").value.toString())
+                    contentImg.setImageURI(Uri.parse(snapshot.child("imageRef").value.toString()))
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        publishB.setOnClickListener {
+            val hm = HashMap<String,Any>()
+            hm["title"]= title.text.toString()
+            hm["desc"]= contentDesc.text.toString()
+            hm["url"] = contentUrl.text.toString()
+            reference.articleReference.child(contentUid).updateChildren(hm).addOnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(activity,"Article Updated",Toast.LENGTH_LONG).show()
+                    activity?.supportFragmentManager?.popBackStack()
+                }
+            }
+        }
+
+        contentImg.setOnClickListener {
+            val gallery = Intent()
+            gallery.setAction(Intent.ACTION_GET_CONTENT)
+            gallery.setType("image/*")
+            startActivityForResult(gallery,galleryPick)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == galleryPick && resultCode == Activity.RESULT_OK && data!=null) {
+            imgUri = data.data!!
+            uploadImageToDatabase(reference,reference.currentUserId,imgUri)
+        }
+    }
+
+    private fun uploadImageToDatabase(reference: FirebasePresenter, currentUserId: String?, imgUri: Uri) {
+        val resultUri = imgUri
+        val path = reference.contentPostRef.child(contentUid + ".jpg")
+        path.putFile(resultUri).addOnCompleteListener {
+            if(it.isSuccessful){
+                path.downloadUrl.addOnSuccessListener {
+                    val downloadUrl = it.toString()
+                    reference.articleReference.child(contentUid).child("imageRef").setValue(downloadUrl).addOnCompleteListener {
+                        if(it.isSuccessful) Toast.makeText(activity,"$downloadUrl",Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else Toast.makeText(activity,"Error: ${it.exception?.message}",Toast.LENGTH_LONG).show()
+        }
+        contentImg.setImageURI(imgUri)
+    }
+
+    private fun initializeValues(view: View) {
+        title = view.findViewById(R.id.contentEditTitle)
+        contentImg = view.findViewById(R.id.contentEditImage)
+        contentDesc = view.findViewById(R.id.contentEditDesc)
+        publishB = view.findViewById(R.id.publishEditContent)
+        contentUrl = view.findViewById(R.id.contentEditUrl)
+        contentRG = view.findViewById(R.id.contentEditRG)
+
+        reference = FirebasePresenter(view)
+    }
+}
